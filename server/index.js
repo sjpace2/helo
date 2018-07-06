17e8332790527a760c4512569f1bc2c2d24088db
Stephen Pace <stephen@Stephens-MacBook-Pro.local>
2018-07-06 10:35:49 -0600

    setup complete

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ctrl = require('./controller');
+const massive = require('massive');
+require('dotenv').config();
+const { CONNECTION_STRING, SESSION_SECRET } = process.env;
+
 
 const PORT = 3005;
 const app = express();
 app.use(bodyParser.json());
+
+massive(CONNECTION_STRING)
+    .then( db => {
+        app.set('db', db)
+        console.log('DB Connected')
+    }).catch( err => console.log(err));
 
 app.listen( PORT || 3005, ()=> console.log(`Listening on port ${PORT || 3005}`));
