065419225d54306120c0178f52081827f34ca6d6
Stephen Pace <stephen@Stephens-MacBook-Pro.local>
2018-07-06 12:43:11 -0600

    step 3 works

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default function Nav (props) {
-   
+
+function Nav (props) {
+  
    
     return (
        <div>
@@ -9,7 +11,21 @@ export default function Nav (props) {
         <button>Home</button>
         <button>New Post</button>
         <button>Logout</button>
+            <div>
+                <h4>
+                    {props.username}
+                    {props.profile_pic}
+                </h4>
+            </div>
        </div>
    )
 }
 
+function mapStateToProps (state) {
+    return {
+        username: state.username,
+        profile_pic: state.profile_pic
+    }
+}
+
+export default connect(mapStateToProps)(Nav)
---
+++ src/ducks/reducer.js
@@ -0,0 +1,12 @@
+let initialState = {
+    username: 'asdfasdf',
+    id: '',
+    profile_pic: ''
+}
+
+export default function reducer( state = initialState, action) {
+    switch(action.type) {
+        default: 
+            return state;
+    }
+}
